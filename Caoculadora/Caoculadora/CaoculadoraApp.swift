d24ddb9ed9972836174becb3a21cd46ddc2fc9fd
Raquel Ribeiro <raquelribeirohf@gmail.com>
2024-05-07 11:30:18 -0300

    Cria projeto no xcode da caoculadora

---
+++ Caoculadora/Caoculadora/CaoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraApp.swift
+//  Caoculadora
+//
+//  Created by Raquel Ribeiro Hatem de Farias on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
